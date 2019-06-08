44724d88c9d17a27795759c205cab5156de8bbd5
Nebiyat Sirak <Nebiyat.sirak@outlook.com>
2019-06-08 09:36:16 +0200

    createStreams

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends Component {
 	renderInput({ input, label, meta }) {
@@ -24,9 +26,10 @@ class StreamCreate extends Component {
 		);
 	}
 
-	onSubmit(formValues) {
+	onSubmit = (formValues) => {
 		// console.log(formValues);
-	}
+		this.props.createStream(formValues);
+	};
 
 	render() {
 		// console.log(this.props);
@@ -40,7 +43,7 @@ class StreamCreate extends Component {
 	}
 }
 
-const validate = formValues => {
+const validate = (formValues) => {
 	const errors = {};
 	if (!formValues.title) {
 		// only ran if the user didn't run a title
@@ -54,7 +57,9 @@ const validate = formValues => {
 	return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
 	form: 'streamCreate',
 	validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
